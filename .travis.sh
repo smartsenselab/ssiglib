068a3107512af8a9c2f9321be2a1578bb9e4d0ad
Antonio Carlos <acnazarejr@gmail.com>
2015-03-15 18:26:27 -0300

    Add automatic script for travis

---
+++ .travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SSF_DIR=`pwd`
+BUILD_DIR=$SSF_DIR/build
+
+function build()
+{
+  mkdir $BUILD_DIR && cd $BUILD_DIR
+  cmake -DBUILD_TESTS=OFF $SSF_DIR
+  make -j2
+}
+
+function test()
+{
+  mkdir $BUILD_DIR && cd $BUILD_DIR
+  cmake -DBUILD_TESTS=ON $SSF_DIR
+  make ssf_core -j3
+  make test_core
+  make test -j3
+}
+
+case $TASK in
+  build ) build;;
+  test ) test;;
+esac
