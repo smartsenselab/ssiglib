521bc539074e55d2170eb760a3f07b523cea4af0
rcoutinho <romulocoutinho16@gmail.com>
2015-03-27 17:47:07 -0300

    Added statistics structure components.

---
+++ components/statistics/include/statistics/pls.hpp
@@ -0,0 +1,21 @@
+#ifndef _SSF_STATISTICS_PLS_HPP_
+#define _SSF_STATISTICS_PLS_HPP_
+
+namespace ssf{
+
+	class PLS{
+	
+	public:
+		PLS(void);
+		virtual ~PLS(void);
+		PLS(const PLS& rhs);
+		PLS& operator=(const PLS& rhs);
+
+	private:
+		//private members
+
+	};
+
+}
+
+#endif // !_SSF_STATISTICS_PLS_HPP_
---
+++ components/statistics/src/pls.cpp
@@ -0,0 +1,25 @@
+#include "statistics/pls.hpp"
+
+namespace ssf{
+
+	PLS::PLS(){
+		//Constructor
+	}
+
+	PLS::~PLS(){
+		//Destructor
+	}
+
+	PLS::PLS(const PLS& rhs){
+		//Constructor Copy
+	}
+
+	PLS& PLS::operator=(const PLS& rhs){
+		if (this != &rhs){
+			//code here
+		}
+	    return *this;
+	}
+
+}
+
