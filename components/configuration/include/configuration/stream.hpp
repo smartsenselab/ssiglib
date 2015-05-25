2e2a44c44dc68ee05916a0ce06fbe7e5889a3123
Antonio Carlos <acnazarejr@gmail.com>
2015-05-25 08:18:12 -0300

    Try fix travis problem

--- components/configuration/include/configuration/stream.hpp
+++ components/configuration/include/configuration/stream.hpp
@@ -1,33 +1,33 @@
-#ifndef _SSF_API_STREAM_HPP_
-#define _SSF_API_STREAM_HPP_
-
-#include <string>
-
-#include "configuration/configuration_defs.hpp"
-
-namespace ssf{
-
-	class Stream{
-	
-	public:
-        CONFIG_EXPORT Stream();
-		CONFIG_EXPORT Stream(const std::string& moduleIDProvider, const std::string& outputProvider, const std::string& moduleIDReceiver, const std::string inputReceiver);
-		CONFIG_EXPORT virtual ~Stream(void);
-		CONFIG_EXPORT Stream(const Stream& rhs);
-		CONFIG_EXPORT Stream& operator=(const Stream& rhs);
-        
-		CONFIG_EXPORT std::string getModuleIDProvider();
-		CONFIG_EXPORT std::string getModuleIDReceiver();
-		CONFIG_EXPORT std::string getOutputProvider();
-		CONFIG_EXPORT std::string getInputReceiver();
-
-	private:
-        std::string mModuleIDProvider;
-        std::string mModuleIDReceiver;
-        std::string mOutputProvider;
-        std::string mInputReceiver;
-	};
-
-}
-
-#endif // !_SSF_API_STREAM_HPP_
+//#ifndef _SSF_API_STREAM_HPP_
+//#define _SSF_API_STREAM_HPP_
+//
+//#include <string>
+//
+//#include "configuration/configuration_defs.hpp"
+//
+//namespace ssf{
+//
+//	class Stream{
+//	
+//	public:
+//        CONFIG_EXPORT Stream();
+//		CONFIG_EXPORT Stream(const std::string& moduleIDProvider, const std::string& outputProvider, const std::string& moduleIDReceiver, const std::string inputReceiver);
+//		CONFIG_EXPORT virtual ~Stream(void);
+//		CONFIG_EXPORT Stream(const Stream& rhs);
+//		CONFIG_EXPORT Stream& operator=(const Stream& rhs);
+//        
+//		CONFIG_EXPORT std::string getModuleIDProvider();
+//		CONFIG_EXPORT std::string getModuleIDReceiver();
+//		CONFIG_EXPORT std::string getOutputProvider();
+//		CONFIG_EXPORT std::string getInputReceiver();
+//
+//	private:
+//        std::string mModuleIDProvider;
+//        std::string mModuleIDReceiver;
+//        std::string mOutputProvider;
+//        std::string mInputReceiver;
+//	};
+//
+//}
+//
+//#endif // !_SSF_API_STREAM_HPP_
