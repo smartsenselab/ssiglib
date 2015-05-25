f0c73eb7f42b0ad24801bec38b14743ce353297d
Antonio Carlos <acnazarejr@gmail.com>
2015-05-25 09:04:57 -0300

    Include Resource (TEMP) and Plugins

--- components/core/test/subtest_plugins/plugin_instance.cpp
+++ components/core/test/subtest_plugins/plugin_instance.cpp
@@ -1,19 +1,19 @@
-//#include "plugin_instance.hpp"
-//
-//PLUGIN_DEFINITION(ResourceSpecialization)
-//
-//void ResourceSpecialization::initialize(){
-//	this->setAuthor("Antonio");
-//	this->setAuthorEmail("antonio@ssig.br");
-//	this->setDescription("resource_description");
-//	this->setRequiredVersion(2, 0);
-//
-//	this->PARAMETERS.addParameter("paramInt", "description_test_int", ssf::ParamType::INTEGER);
-//	this->PARAMETERS.addParameter("paramLong", "description_test_long", ssf::ParamType::INTEGER);
-//	this->PARAMETERS.addParameter("paramFloat", "description_test_float", ssf::ParamType::REAL);
-//	this->PARAMETERS.addParameter("paramDouble", "description_test_double", ssf::ParamType::REAL);
-//	this->PARAMETERS.addParameter("paramBool", "description_test_bool", ssf::ParamType::BOOL);
-//	this->PARAMETERS.addParameter("paramString", "description_test_string", ssf::ParamType::STRING);
-//	this->PARAMETERS.addParameter("paramFile", "description_test_file", ssf::ParamType::FILE_HANDLE);
-//	this->PARAMETERS.addParameter("paramDirectory", "description_test_directory", ssf::ParamType::DIRECTORY_HANDLE);
-//}
+#include "plugin_instance.hpp"
+
+PLUGIN_DEFINITION(ResourceSpecialization)
+
+void ResourceSpecialization::initialize(){
+	/*this->setAuthor("Antonio");
+	this->setAuthorEmail("antonio@ssig.br");
+	this->setDescription("resource_description");
+	this->setRequiredVersion(2, 0);
+
+	this->PARAMETERS.addParameter("paramInt", "description_test_int", ssf::ParamType::INTEGER);
+	this->PARAMETERS.addParameter("paramLong", "description_test_long", ssf::ParamType::INTEGER);
+	this->PARAMETERS.addParameter("paramFloat", "description_test_float", ssf::ParamType::REAL);
+	this->PARAMETERS.addParameter("paramDouble", "description_test_double", ssf::ParamType::REAL);
+	this->PARAMETERS.addParameter("paramBool", "description_test_bool", ssf::ParamType::BOOL);
+	this->PARAMETERS.addParameter("paramString", "description_test_string", ssf::ParamType::STRING);
+	this->PARAMETERS.addParameter("paramFile", "description_test_file", ssf::ParamType::FILE_HANDLE);
+	this->PARAMETERS.addParameter("paramDirectory", "description_test_directory", ssf::ParamType::DIRECTORY_HANDLE);*/
+}
--- components/core/test/subtest_plugins/plugin_instance.hpp
+++ components/core/test/subtest_plugins/plugin_instance.hpp
@@ -1,13 +1,13 @@
-//#include "core/plugin_defs.hpp"
-//#include "core/resource.hpp"
-//
-//class ResourceSpecialization : public ssf::Resource{
-//public:
-//	std::string getName() { return "RESOURCE_SPECIALIZATION"; }
-//	std::string getType() { return "TYPE_RESOURCE_SPECIALIZATION"; }
-//
-//	void initialize();
-//
-//};
-//
-//PLUGIN_DECLARATION(ResourceSpecialization)
+#include "core/plugin_defs.hpp"
+#include "core/resource.hpp"
+
+class ResourceSpecialization : public ssf::Resource{
+public:
+	std::string getName() { return "RESOURCE_SPECIALIZATION"; }
+	std::string getType() { return "TYPE_RESOURCE_SPECIALIZATION"; }
+
+	void initialize();
+
+};
+
+PLUGIN_DECLARATION(ResourceSpecialization)
