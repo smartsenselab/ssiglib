f0fa136b157f9b964d29c0d2c1d916c442a58692
Antonio Carlos <acnazarejr@gmail.com>
2015-05-25 08:51:25 -0300

    Include other tests

--- components/core/test/test_basic_structures.cpp
+++ components/core/test/test_basic_structures.cpp
@@ -1,61 +1,61 @@
-//#include <gtest/gtest.h>
-//#include <fstream>
-//
-//#include <opencv/cv.h>
-//
-//#include <core/point2d.hpp>
-//#include <core/point3d.hpp>
-//#include <core/cube.hpp>
-//#include <core/rect.hpp>
-//
-//TEST(Point2D, handles) {
-//	ssf::Point2Di point_i;
-//	ssf::Point2Df point_f;
-//	ssf::Point2Dd point_d;
-//	ssf::Point2D<long> point_l;
-//	
-//	EXPECT_EQ(0, point_i.getX());
-//	EXPECT_EQ(0, point_i.getY());
-//
-//	point_i.set(11, 11);
-//	cv::Point point_cv = point_i.toOpenCVPoint();
-//	EXPECT_EQ(11, point_cv.x);
-//	EXPECT_EQ(11, point_cv.y);
-//
-//	point_cv = cv::Point(22, 22);
-//	point_i.set(point_cv);
-//
-//	EXPECT_EQ(22, point_i.getX());
-//	EXPECT_EQ(22, point_i.getY());
-//
-//	ssf::Point2Di point_i_b(point_cv);
-//	EXPECT_TRUE(point_i == point_i_b);
-//	EXPECT_FALSE(point_i != point_i_b);
-//
-//}
-//
-//TEST(Point3D, handles) {
-//	ssf::Point3Di point_i;
-//	ssf::Point3Df point_f;
-//	ssf::Point3Dd point_d;
-//	ssf::Point3D<long> point_l;
-//
-//	EXPECT_EQ(0, point_i.getX());
-//	EXPECT_EQ(0, point_i.getY());
-//	EXPECT_EQ(0, point_i.getZ());
-//}
-//
-//TEST(Rect, handles) {
-//	ssf::Rect<int> rect;
-//
-//	EXPECT_EQ(0, rect.getPositionX());
-//	EXPECT_EQ(0, rect.getPositionY());
-//}
-//
-//TEST(Cube, handles) {
-//	ssf::Cube<int> cube;
-//	
-//	EXPECT_EQ(0, cube.getPositionX());
-//	EXPECT_EQ(0, cube.getPositionY());
-//	EXPECT_EQ(0, cube.getPositionZ());
-//}
+#include <gtest/gtest.h>
+#include <fstream>
+
+#include <opencv/cv.h>
+
+#include <core/point2d.hpp>
+#include <core/point3d.hpp>
+#include <core/cube.hpp>
+#include <core/rect.hpp>
+
+TEST(Point2D, handles) {
+	ssf::Point2Di point_i;
+	ssf::Point2Df point_f;
+	ssf::Point2Dd point_d;
+	ssf::Point2D<long> point_l;
+	
+	EXPECT_EQ(0, point_i.getX());
+	EXPECT_EQ(0, point_i.getY());
+
+	point_i.set(11, 11);
+	cv::Point point_cv = point_i.toOpenCVPoint();
+	EXPECT_EQ(11, point_cv.x);
+	EXPECT_EQ(11, point_cv.y);
+
+	point_cv = cv::Point(22, 22);
+	point_i.set(point_cv);
+
+	EXPECT_EQ(22, point_i.getX());
+	EXPECT_EQ(22, point_i.getY());
+
+	ssf::Point2Di point_i_b(point_cv);
+	EXPECT_TRUE(point_i == point_i_b);
+	EXPECT_FALSE(point_i != point_i_b);
+
+}
+
+TEST(Point3D, handles) {
+	ssf::Point3Di point_i;
+	ssf::Point3Df point_f;
+	ssf::Point3Dd point_d;
+	ssf::Point3D<long> point_l;
+
+	EXPECT_EQ(0, point_i.getX());
+	EXPECT_EQ(0, point_i.getY());
+	EXPECT_EQ(0, point_i.getZ());
+}
+
+TEST(Rect, handles) {
+	ssf::Rect<int> rect;
+
+	EXPECT_EQ(0, rect.getPositionX());
+	EXPECT_EQ(0, rect.getPositionY());
+}
+
+TEST(Cube, handles) {
+	ssf::Cube<int> cube;
+	
+	EXPECT_EQ(0, cube.getPositionX());
+	EXPECT_EQ(0, cube.getPositionY());
+	EXPECT_EQ(0, cube.getPositionZ());
+}
--- components/core/test/test_files.cpp
+++ components/core/test/test_files.cpp
@@ -1,93 +1,93 @@
-//#include <gtest/gtest.h>
-//#include <fstream>
-//
-//#include <core/file_handle.hpp>
-//#include <core/directory_handle.hpp>
-//
-//TEST(FileHandle, constructor) {
-//	EXPECT_NO_THROW(ssf::FileHandle InvalidFile("invalid.txt"));
-//	EXPECT_NO_THROW(ssf::FileHandle ValidFile("test_file.txt"));
-//}
-//
-//TEST(FileHandle, exists){
-//	EXPECT_TRUE(ssf::FileHandle::exists("test_file.txt"));
-//	EXPECT_FALSE(ssf::FileHandle::exists("invalid.txt"));
-//}
-//
-//TEST(FileHandle, hasExtension) {
-//	ssf::FileHandle fileHandle("test_file.txt");
-//	EXPECT_TRUE(fileHandle.hasExtension());
-//}
-//
-//TEST(FileHandle, erase) {
-//	std::ofstream tempFile("temp_file.txt");
-//	tempFile << "temp" << std::endl;
-//	tempFile.close();
-//
-//	std::ofstream tempFile2("temp_file2.txt");
-//	tempFile2 << "temp" << std::endl;
-//	tempFile2.close();
-//
-//	ssf::FileHandle fileHandle("temp_file.txt");
-//	EXPECT_TRUE(fileHandle.erase());
-//	EXPECT_TRUE(ssf::FileHandle::erase("temp_file2.txt"));
-//	EXPECT_FALSE(ssf::FileHandle::erase("invalid_file.txt"));
-//}
-//
-//TEST(FileHandle, strings) {
-//	ssf::FileHandle fileHandle("test_file.txt");
-//	EXPECT_STREQ("test_file", fileHandle.getSimpleName().c_str());
-//}
-//
-//TEST(FileHandle, copy) {
-//	ssf::FileHandle fileHandle1("test_file.txt");
-//	ssf::FileHandle fileHandle2(fileHandle1);
-//	ssf::FileHandle fileHandle3 = fileHandle2;
-//
-//	EXPECT_STREQ("test_file", fileHandle2.getSimpleName().c_str());
-//	EXPECT_STREQ("test_file", fileHandle3.getSimpleName().c_str());
-//}
-//
-/////////////////
-//
-//TEST(DirectoryHandle, constructor){
-//	EXPECT_NO_THROW(ssf::DirectoryHandle InvalidFile("invalid_folder"));
-//	EXPECT_NO_THROW(ssf::DirectoryHandle ValidFile("folder_test"));
-//}
-//
-//TEST(DirectoryHandle, exists){
-//	EXPECT_TRUE(ssf::DirectoryHandle::exists("folder_test"));
-//	EXPECT_FALSE(ssf::DirectoryHandle::exists("invalid_folder"));
-//}
-//
-//TEST(DirectoryHandle, erase) {
-//	//Aims a unnecessary link with a library to folder creation,
-//	//the valid folder erase test was implemented in "creation" test.
-//	EXPECT_FALSE(ssf::DirectoryHandle::erase("invalid_folder"));
-//}
-//
-//TEST(DirectoryHandle, lists) {
-//	ssf::DirectoryHandle directoryHandle("folder_test");
-//	EXPECT_EQ(3, directoryHandle.listSubDirectories().size());
-//}
-//
-//TEST(DirectoryHandle, create) {
-//	ssf::DirectoryHandle::create("temp_folder");
-//	EXPECT_NO_THROW(ssf::DirectoryHandle ValidFile("temp_folder"));
-//	EXPECT_TRUE(ssf::DirectoryHandle::exists("temp_folder"));
-//	EXPECT_TRUE(ssf::DirectoryHandle::erase("temp_folder"));
-//}
-//
-//TEST(DirectoryHandle, strings) {
-//	ssf::DirectoryHandle directoryHandle("folder_test");
-//	EXPECT_STREQ("folder_test", directoryHandle.getSimpleName().c_str());
-//}
-//
-//TEST(DirectoryHandle, copy) {
-//	ssf::DirectoryHandle directoryHandle1("folder_test");
-//	ssf::DirectoryHandle directoryHandle2(directoryHandle1);
-//	ssf::DirectoryHandle directoryHandle3 = directoryHandle2;
-//
-//	EXPECT_STREQ("folder_test", directoryHandle2.getSimpleName().c_str());
-//	EXPECT_STREQ("folder_test", directoryHandle3.getSimpleName().c_str());
-//}
+#include <gtest/gtest.h>
+#include <fstream>
+
+#include <core/file_handle.hpp>
+#include <core/directory_handle.hpp>
+
+TEST(FileHandle, constructor) {
+	EXPECT_NO_THROW(ssf::FileHandle InvalidFile("invalid.txt"));
+	EXPECT_NO_THROW(ssf::FileHandle ValidFile("test_file.txt"));
+}
+
+TEST(FileHandle, exists){
+	EXPECT_TRUE(ssf::FileHandle::exists("test_file.txt"));
+	EXPECT_FALSE(ssf::FileHandle::exists("invalid.txt"));
+}
+
+TEST(FileHandle, hasExtension) {
+	ssf::FileHandle fileHandle("test_file.txt");
+	EXPECT_TRUE(fileHandle.hasExtension());
+}
+
+TEST(FileHandle, erase) {
+	std::ofstream tempFile("temp_file.txt");
+	tempFile << "temp" << std::endl;
+	tempFile.close();
+
+	std::ofstream tempFile2("temp_file2.txt");
+	tempFile2 << "temp" << std::endl;
+	tempFile2.close();
+
+	ssf::FileHandle fileHandle("temp_file.txt");
+	EXPECT_TRUE(fileHandle.erase());
+	EXPECT_TRUE(ssf::FileHandle::erase("temp_file2.txt"));
+	EXPECT_FALSE(ssf::FileHandle::erase("invalid_file.txt"));
+}
+
+TEST(FileHandle, strings) {
+	ssf::FileHandle fileHandle("test_file.txt");
+	EXPECT_STREQ("test_file", fileHandle.getSimpleName().c_str());
+}
+
+TEST(FileHandle, copy) {
+	ssf::FileHandle fileHandle1("test_file.txt");
+	ssf::FileHandle fileHandle2(fileHandle1);
+	ssf::FileHandle fileHandle3 = fileHandle2;
+
+	EXPECT_STREQ("test_file", fileHandle2.getSimpleName().c_str());
+	EXPECT_STREQ("test_file", fileHandle3.getSimpleName().c_str());
+}
+
+///////////////
+
+TEST(DirectoryHandle, constructor){
+	EXPECT_NO_THROW(ssf::DirectoryHandle InvalidFile("invalid_folder"));
+	EXPECT_NO_THROW(ssf::DirectoryHandle ValidFile("folder_test"));
+}
+
+TEST(DirectoryHandle, exists){
+	EXPECT_TRUE(ssf::DirectoryHandle::exists("folder_test"));
+	EXPECT_FALSE(ssf::DirectoryHandle::exists("invalid_folder"));
+}
+
+TEST(DirectoryHandle, erase) {
+	//Aims a unnecessary link with a library to folder creation,
+	//the valid folder erase test was implemented in "creation" test.
+	EXPECT_FALSE(ssf::DirectoryHandle::erase("invalid_folder"));
+}
+
+TEST(DirectoryHandle, lists) {
+	ssf::DirectoryHandle directoryHandle("folder_test");
+	EXPECT_EQ(3, directoryHandle.listSubDirectories().size());
+}
+
+TEST(DirectoryHandle, create) {
+	ssf::DirectoryHandle::create("temp_folder");
+	EXPECT_NO_THROW(ssf::DirectoryHandle ValidFile("temp_folder"));
+	EXPECT_TRUE(ssf::DirectoryHandle::exists("temp_folder"));
+	EXPECT_TRUE(ssf::DirectoryHandle::erase("temp_folder"));
+}
+
+TEST(DirectoryHandle, strings) {
+	ssf::DirectoryHandle directoryHandle("folder_test");
+	EXPECT_STREQ("folder_test", directoryHandle.getSimpleName().c_str());
+}
+
+TEST(DirectoryHandle, copy) {
+	ssf::DirectoryHandle directoryHandle1("folder_test");
+	ssf::DirectoryHandle directoryHandle2(directoryHandle1);
+	ssf::DirectoryHandle directoryHandle3 = directoryHandle2;
+
+	EXPECT_STREQ("folder_test", directoryHandle2.getSimpleName().c_str());
+	EXPECT_STREQ("folder_test", directoryHandle3.getSimpleName().c_str());
+}
