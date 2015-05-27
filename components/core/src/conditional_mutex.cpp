b68ea41e3f672e180b2712a602adf526c5c98839
Antonio Carlos <acnazarejr@gmail.com>
2015-05-27 19:22:50 -0300

    Add Conditional Mutex

--- components/core/src/conditional_mutex.cpp
+++ components/core/src/conditional_mutex.cpp
@@ -1,32 +1,32 @@
-//#include "core/conditional_mutex.hpp"
-//
-//namespace ssf{
-//
-//	ConditionalMutex::ConditionalMutex(const bool& ready /*= false*/){
-//		std::unique_lock<std::mutex> ulk(this->mMutex);
-//		this->mReadyStatus = ready;
-//	}
-//
-//	ConditionalMutex::~ConditionalMutex(){
-//		//Destructor
-//	}
-//
-//	void ConditionalMutex::unready(){
-//		std::unique_lock<std::mutex> ulk(this->mMutex);
-//		this->mReadyStatus = false;
-//		this->mConditionVariable.notify_all();
-//	}
-//
-//	void ConditionalMutex::ready(){
-//		std::unique_lock<std::mutex> ulk(this->mMutex);
-//		this->mReadyStatus = true;
-//		this->mConditionVariable.notify_all();
-//	}
-//
-//	void ConditionalMutex::waitForReady(){
-//		std::unique_lock<std::mutex> ulk(this->mMutex);
-//		while (!this->mReadyStatus)
-//			this->mConditionVariable.wait(ulk);
-//	}
-//
-//}
+#include "core/conditional_mutex.hpp"
+
+namespace ssf{
+
+	ConditionalMutex::ConditionalMutex(const bool& ready /*= false*/){
+		std::unique_lock<std::mutex> ulk(this->mMutex);
+		this->mReadyStatus = ready;
+	}
+
+	ConditionalMutex::~ConditionalMutex(){
+		//Destructor
+	}
+
+	void ConditionalMutex::unready(){
+		std::unique_lock<std::mutex> ulk(this->mMutex);
+		this->mReadyStatus = false;
+		this->mConditionVariable.notify_all();
+	}
+
+	void ConditionalMutex::ready(){
+		std::unique_lock<std::mutex> ulk(this->mMutex);
+		this->mReadyStatus = true;
+		this->mConditionVariable.notify_all();
+	}
+
+	void ConditionalMutex::waitForReady(){
+		std::unique_lock<std::mutex> ulk(this->mMutex);
+		while (!this->mReadyStatus)
+			this->mConditionVariable.wait(ulk);
+	}
+
+}
