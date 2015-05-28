cd9992b7eb1a82071c61d6fb980eed8f1b2ecdab
Cesar Augusto <amfcesar@gmail.com>
2015-05-28 15:10:15 -0300

    The Methods send Tilt / Pan / are working perfectly. The method of reaching the command was executed also is running. The get, are not possible to implement this camera.

--- components/cameras/app/main.cpp
+++ components/cameras/app/main.cpp
@@ -1,13 +1,17 @@
 
-#include <cameras\video_camera_PTZ_Trend_Net7865.hpp>
+#include <cameras/ip_video_camera_PTZ_Trend_Net7865.hpp>
 
 int main(){
 
-	ssf::VideoCameraPTZTrendNet7865 camera("172.22.22.210");
+	ssf::IPVideoCameraPTZTrendNet7865 camera("172.22.22.210");
 
-	camera.setPAN(30);
+	//camera.check();
+	//camera.setPAN(-10);
+	//camera.setPAN(30);
+	//camera.goHome();
+	//camera.setZoom(-0.1);
 	//camera.setPAN(-100);
-	//camera.setTILT(100);
+	camera.setTILT(240);
 
 	return 0;
 }
