392424e6c0cc1f2aa8f8ad689b876e243acd8aa1
ricardo <klossricardo@gmail.com>
2015-09-09 19:56:28 -0300

    Signed Off By <ricardo - rbk@dcc.ufmg.br>

--- opencv.sh
+++ opencv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir opencv
-cd opencv
+mkdir /home/travis/build/rbkloss/opencv/
+cd /home/travis/build/rbkloss/opencv/
 git clone https://github.com/Itseez/opencv.git
 
 mkdir release
@@ -9,7 +9,7 @@ cd release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_OPENGL=ON ..
 
-make -j1 # -j8 runs 8 jobs in parallel.
+make # -j8 runs 8 jobs in parallel.
          # Change 8 to number of hardware threads available.
 sudo make install
 
@@ -18,4 +18,4 @@ sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
 echo "OpenCV installed."
 
-cd ../../
+cd /home/travis/build/rbkloss/ssf/
