d10d85d83fa2f818f50b29ce7adbfd8a492fad8e
ricardo <klossricardo@gmail.com>
2015-09-09 20:17:11 -0300

    Travis script from https://gist.github.com/cgddrd/9d397fda0d72fe617c0e
    Signed Off By <ricardo - rbk@dcc.ufmg.br>

--- opencv.sh
+++ opencv.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+sudo apt-get install -qq cmake
+sudo apt-get install -qq libboost-filesystem1.54-dev libboost-log1.54-dev libboost-date-time1.54-dev libboost-thread1.54-dev
+sudo apt-get install build-essential
+sudo apt-get install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -qq gcc-4.8 g++-4.8
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 90 
+
 mkdir /home/travis/build/rbkloss/opencv/
 cd /home/travis/build/rbkloss/opencv/
 git clone https://github.com/Itseez/opencv.git
