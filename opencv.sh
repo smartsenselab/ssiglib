f73f2c9780679e1d0c7b1072299df3a83f1624f0
ricardo <klossricardo@gmail.com>
2015-09-09 19:33:25 -0300

    Please Travis be a good boy!
    Signed Off By <ricardo - rbk@dcc.ufmg.br>

--- opencv.sh
+++ opencv.sh
@@ -2,6 +2,7 @@
 sudo apt-get install build-essential
 sudo apt-get install -qq gcc-4.8 g++-4.8
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 90 
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
