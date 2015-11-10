1e0e473e93f6186fd51a82f2450b826def31d196
acnazarejr <acnazarejr@gmail.com>
2015-11-10 16:42:15 -0200

    Travis update

--- .travis.sh
+++ .travis.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-SSF_DIR=`pwd`
-BUILD_DIR=$SSF_DIR/build
-
-function build()
-{
-  mkdir $BUILD_DIR && cd $BUILD_DIR
-  cmake -DBUILD_TESTS=OFF $SSF_DIR
-  make
-}
-
-function test()
-{
-  mkdir $BUILD_DIR && cd $BUILD_DIR
-  cmake -DBUILD_TESTS=ON $SSF_DIR
-  make
-  make test  ARGS="--output-on-failure"
-}
-
-function coverage()
-{
-  mkdir $BUILD_DIR && cd $BUILD_DIR
-  cmake -DBUILD_TESTS=ON -DENABLE_COVERAGE=ON $SSF_DIR
-  make
-  make test ARGS="--output-on-failure"
-}
-
-case $TASK in
-  build ) build;;
-  test ) test;;
-  coverage ) coverage;;
-esac
+# SSF_DIR=`pwd`
+# BUILD_DIR=$SSF_DIR/build
+#
+# function build()
+# {
+#   mkdir $BUILD_DIR && cd $BUILD_DIR
+#   cmake -DBUILD_TESTS=OFF $SSF_DIR
+#   make
+# }
+#
+# function test()
+# {
+#   mkdir $BUILD_DIR && cd $BUILD_DIR
+#   cmake -DBUILD_TESTS=ON $SSF_DIR
+#   make
+#   make test  ARGS="--output-on-failure"
+# }
+#
+# function coverage()
+# {
+#   mkdir $BUILD_DIR && cd $BUILD_DIR
+#   cmake -DBUILD_TESTS=ON -DENABLE_COVERAGE=ON $SSF_DIR
+#   make
+#   make test ARGS="--output-on-failure"
+# }
+#
+# case $TASK in
+#   build ) build;;
+#   test ) test;;
+#   coverage ) coverage;;
+# esac
