d1f868a27c6fcfa18fe3dbffc4399e05e23c1f9d
ricardo <klossricardo@gmail.com>
2015-12-04 22:47:12 -0200

    Signed Off By <ricardo - rbk@dcc.ufmg.br>

--- scripts/data/ssf_template_header.hpp
+++ scripts/data/ssf_template_header.hpp
@@ -1,21 +1,17 @@
-#ifndef _SSF_{componentUpper}_{fileNameUpper}_HPP_
-#define _SSF_{componentUpper}_{fileNameUpper}_HPP_
+#ifndef _SSIG_{componentUpper}_{fileNameUpper}_HPP_
+#define _SSIG_{componentUpper}_{fileNameUpper}_HPP_
 
-namespace ssf{{
+namespace ssig{{
+class {className}{{
 
-	class {className}{{
-	
-	public:
-		{className}(void);
-		virtual ~{className}(void);
-		{className}(const {className}& rhs);
-		{className}& operator=(const {className}& rhs);
+ public:
+  {className}(void);
+  virtual ~{className}(void);
+  {className}(const {className}& rhs);
+  {className}& operator=(const {className}& rhs);
 
-	private:
-		//private members
-
-	}};
-
-}}
-
-#endif // !_SSF_{componentUpper}_{fileNameUpper}_HPP_
+ private:
+  // private members
+}};
+}}  // ssig namespace
+#endif  // !_SSF_{componentUpper}_{fileNameUpper}_HPP_
--- scripts/data/ssf_template_source.cpp
+++ scripts/data/ssf_template_source.cpp
@@ -1,25 +1,23 @@
 #include "{component}/{fileName}.hpp"
 
-namespace ssf{{
-
-	{className}::{className}(){{
-		//Constructor
-	}}
-
-	{className}::~{className}(){{
-		//Destructor
-	}}
-
-	{className}::{className}(const {className}& rhs){{
-		//Constructor Copy
-	}}
-
-	{className}& {className}::operator=(const {className}& rhs){{
-		if (this != &rhs){{
-			//code here
-		}}
-	    return *this;
-	}}
-
+namespace ssig{{
+{className}::{className}(){{
+  //Constructor
 }}
+
+{className}::~{className}(){{
+  //Destructor
+}}
+
+{className}::{className}(const {className}& rhs){{
+  //Constructor Copy
+}}
+
+{className}& {className}::operator=(const {className}& rhs){{
+  if (this != &rhs){{
+    //code here
+  }}
+  return *this;
+}}
+}}  // ssig namespace
 
