ef21aac75f0da69180b805fb5e9f8cf2907fd7ae
ricardo <klossricardo@gmail.com>
2016-02-01 00:54:16 -0200

    Signed Off By <ricardo - rbk@dcc.ufmg.br>

--- modules/descriptors/test/test_hog_uoccti.cpp
+++ modules/descriptors/test/test_hog_uoccti.cpp
@@ -68,7 +68,7 @@ TEST(HOGUOCCTI, Simple) {
 
   cv::Mat diff = cv::abs(out - expected);
   cv::Mat epsilon(diff.rows, diff.cols, CV_32FC1);
-  epsilon = 2 * FLT_EPSILON;
+  epsilon = static_cast<float>(1e-5);
   cv::Mat cmpson;
   cv::compare(diff, epsilon, cmpson, cv::CMP_LT);
   int diffSum = cv::countNonZero(cmpson);
