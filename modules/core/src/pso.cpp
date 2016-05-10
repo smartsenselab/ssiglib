5a8cb50f0e137d6a0aea782195064bb547ee08ed
ricardo <klossricardo@gmail.com>
2016-05-10 15:51:40 -0300

    Signed Off By <ricardo - rbk@dcc.ufmg.br>

--- modules/core/src/pso.cpp
+++ modules/core/src/pso.cpp
@@ -122,7 +122,7 @@ void PSO::learn(cv::Mat_<float>& input) {
 }
 
 void PSO::iterate() {
-  //#pragma omp parallel for
+  #pragma omp parallel for
   for (int r = 0; r < mPopulationLength; ++r) {
     cv::Mat position = mPopulation.row(r),
         localBest = mLocalBests.row(r),
@@ -132,7 +132,7 @@ void PSO::iterate() {
     float currentUtil = utility(position);
     mLocalUtils[r] = currentUtil;
 
-    //#pragma omp critical(UPDATING)
+    #pragma omp critical(UPDATING)
     if (currentUtil >= mBestUtil) {
       mBestPosition = position.clone();
       mBestUtil = currentUtil;
